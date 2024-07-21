8b68f8e6c90ca6a2301a729e673174cabc1e0a27
Dilip D <dili.d61296@gmail.com>
2024-07-21 20:41:51 +0530

    AMigration command change

--- backend/migrations/migrate.mjs
+++ backend/migrations/migrate.mjs
@@ -1,6 +1,5 @@
-const { exec } = require("child_process");
+import { exec } from "child_process";
 
-// Run Prisma Migrate
 exec("npx prisma migrate deploy", (err, stdout, stderr) => {
   if (err) {
     console.error(`Error running migration: ${stderr}`);
